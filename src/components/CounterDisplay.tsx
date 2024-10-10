27c86aafc947ad08a3a49b2af5c9d80474ff4914
Nikita Lobanov <nikitaalobanovv@gmail.com>
2024-10-09 22:19:17 -0700

    edited styling

--- src/components/CounterDisplay.tsx
+++ src/components/CounterDisplay.tsx
@@ -23,7 +23,7 @@ export default function CounterDisplay() {
   return (
     <div>
       <h2 className='text-4xl font-bold'>Current Count: {count !== null ? count : 'Loading...'}</h2>
-      <button onClick={fetchCount} className="bg-blue-500 hover:bg-blue-700 text-white text-2xl font-semibold py-2 px-4 rounded-lg mt-8">
+      <button onClick={fetchCount} className="bg-sky-200 hover:bg-sky-800 transition-all active:scale-75 text-black hover:text-white text-2xl font-semibold py-2 px-4 rounded-lg mt-8">
         Increment
       </button>
     </div>
