521aab488398fb07210dad36e3a54116df1fc6a1
Nikita Lobanov <nikitaalobanovv@gmail.com>
2024-10-09 22:13:58 -0700

    help

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,17 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import {useEffect } from 'react';
 import CounterDisplay from '@/components/CounterDisplay';
 import { initDb } from '@/database';
 
 export default function Home() {
-  const [count, setCount] = useState(0);
 
   useEffect(() => {
     initDb();
   }, []);
 
 
-  const incrementCounter = async () => {
-    try {
-      const res = await fetch('/api/increment', { method: 'POST' });
-      if (!res.ok) throw new Error('Failed to increment counter');
-      const data = await res.json();
-      setCount(data.value);
-    } catch (error) {
-      console.error('Error incrementing counter:', error);
-    }
-  };
+
 
   return (
     <div className="flex flex-col items-center justify-center bg-slate-900 min-h-screen py-2">
